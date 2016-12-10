c2a3bf0ef270269ce7895500ec61ae4cd060b0a6
Wenjun <wqu1995@gmail.com>
2016-12-10 16:26:39 -0500

    ijij

--- src/ServerHandler.java
+++ src/ServerHandler.java
@@ -15,36 +15,70 @@ public class ServerHandler implements Runnable{
     protected Socket clientSocket = null;
     protected String clientInput;
     protected Date lastChecked;
+    protected BufferedReader input;
+    protected DataOutputStream output;
+    protected boolean isLogout;
+
 
     public ServerHandler(Socket clientSocket){
         this.clientSocket = clientSocket;
     }
-    public void run(){
-        System.out.println("Connection established");
-        try{
-            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
-            clientInput = input.readLine();
-            System.out.println(clientInput);
-            switch(clientInput.split(" ")[0]){
-                case "LOGIN":
-                    String date = input.readLine();
-                    date = date.substring(6,date.length());
-                    System.out.println(date);
-                    DateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
-                    try{
-                        lastChecked = format.parse(date);
-                    }catch (ParseException e){
-                        e.printStackTrace();
-                    }
-                    clientInput = input.readLine();
-                    if(clientInput.isEmpty())
-                        output.writeBytes("IGP 214 No Cnotent\r\n\r\n");
-            }
-            //System.out.println(clientInput);
 
-        }catch (IOException e){
+    public void run() {
+        System.out.println("Connection established");
+        isLogout = false;
+        try {
+             input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             output = new DataOutputStream(clientSocket.getOutputStream());
+        }catch (IOException e) {
             e.printStackTrace();
+        }
+
+
+        while (!isLogout) {
+            try {
+                clientInput = input.readLine();
+                System.out.println(clientInput);
+                switch (clientInput.split(" ")[0]) {
+                    case "LOGIN":
+                        String date = input.readLine();
+                        date = date.substring(6, date.length());
+                        System.out.println(date);
+                        DateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
+                        try {
+                            lastChecked = format.parse(date);
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        clientInput = input.readLine();
+                        if (clientInput.isEmpty())
+                            output.writeBytes("IGP 214 No Content\r\n\r\n");
+                        break;
+                    case "AG":
+                        output.writeBytes("IGP 310 ALL Groups\r\n\r\n");
+                        try {
+                            BufferedReader Groups = new BufferedReader(new FileReader("Groups.txt"));
+                            String line;
+                            while ((line = Groups.readLine()) != null) {
+                                output.writeBytes(line+"\r\n");
+                            }
+                            output.writeBytes("\r\n");
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case "CK":
+                        output.writeBytes("IGP 251 No Update\r\n\r\n");
+                        break;
+                    case "SG":
+
+
+                }
+                //System.out.println(clientInput);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
